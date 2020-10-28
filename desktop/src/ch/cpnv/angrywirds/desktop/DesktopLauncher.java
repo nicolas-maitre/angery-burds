73bbea77372e22a187062b1b2f26ddb8d00476fc
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-10-28 12:01:49 +0100

    added desktop launcher

--- desktop/src/ch/cpnv/angrywirds/desktop/DesktopLauncher.java
+++ desktop/src/ch/cpnv/angrywirds/desktop/DesktopLauncher.java
@@ -2,7 +2,9 @@ package ch.cpnv.angrywirds.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import ch.cpnv.angrywirds.AngryWirds;
+import ch.cpnv.angrywirds.activities.Welcome;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
